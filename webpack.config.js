67fa766e94008f1a8bb3e006cf3cbc754a1ca09c
andipyk <Andiokey123>
2020-05-18 02:41:53 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html'
+})
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel_loader',
+                }
+            }
+        ]
+    }
+}
