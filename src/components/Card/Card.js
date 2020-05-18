a7e2ad68bb55955c6c17b1aca0d6c920cf0401b6
andipyk <Andiokey123>
2020-05-18 15:44:32 +0700

    tampil json dan tambah install bootstrap

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,19 @@
 import React,{Component} from 'react';
 
-class Card extends Component {
-    render(){
-        return <h2>Ini Card</h2>
-    }
+const Card = ({movie}) => {
+    return (
+        <div>
+            <h2>{ `#${movie.ranking} - ${ movie.title } - (${ movie.year })` }</h2>
+            <img src={`srcnp${movie.img.src}`} alt={movie.img.alt} width='200' />
+            <p> { `Distributor : ${movie.distributor}` } </p>
+            <p> {`Amount : ${movie.amount}` } </p>
+        </div>
+    );
 }
+// class Card extends Component {
+//     render(){
+//         return <h2>Ini Card</h2>
+//     }
+// }
 
 export default Card;
--- src/containers/List.js
+++ src/containers/List.js
@@ -1,11 +1,46 @@
 import React, {Component} from 'react';
-
+import data from '../assets/data.json'
 // Component
 import Card from '../components/Card/Card';
 
 class List extends Component {
+    constructor(){
+        super();
+        this.state = {
+            data: [],
+            loading: true,
+        }
+    }
+
+    async componentDidMount(){
+        try {
+            // const movies = await fetch(data);
+            const moviesJSON = await data;
+            console.log(moviesJSON)
+
+            if (moviesJSON) {
+                this.setState({
+                    data: moviesJSON,
+                    loading: false,
+                })
+            }
+                
+        } catch (err) {
+            console.log(err.message);
+        }
+                  
+
+        console.log('end componentDidMount')
+    }
+
     render(){
-       return <Card/>
+       const {data, loading} = this.state;
+
+       if (loading) {
+           return <div>Loading...</div>
+       }
+
+       return data.map(movie => <Card key={movie.id} movie={movie} />)
     }
 }
 
