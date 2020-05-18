a3423347382cebe27827f6c4800c59981e84d701
andipyk <Andiokey123>
2020-05-18 14:05:59 +0700

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 const htmlPlugin = new HtmlWebPackPlugin({
     template: './src/index.html',
-    filename: './index.html'
-})
+    filename: './index.html',
+});
 
 module.exports = {
     module: {
@@ -10,9 +12,10 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel_loader',
+                    loader: 'babel-loader',
                 }
             }
         ]
-    }
-}
+    },
+    plugins: [htmlPlugin],
+};
